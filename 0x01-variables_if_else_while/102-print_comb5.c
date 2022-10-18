d55af0755ebb57cef134f913d6de64ce220a694d
Melvin1Atieno <melvinatieno@gmail.com>
2022-10-18 13:52:53 -0700

    print combination of numbers from 0 to 99 prepend single digits with zero

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,30 +13,15 @@ int main(void)
 	{
 		for (j = 1; j <= 99; j++)
 		{
-			if ((i < j) && (i != j))
+			if (i < j)
 			{
-				if ((i / 10) < 1)
-				{
-					putchar('0');
-					putchar((i % 10) + '0');
-				}
-				else
-				{
-					putchar(((i / 10) % 10) + '0');
-					putchar((i % 10) + '0');
-				}
+				putchar(((i / 10) % 10) + '0');
+				putchar((i % 10) + '0');
 				putchar(' ');
 
-				if ((j / 10) < 1)
-				{
-					putchar('0');
-					putchar((j % 10) + '0');
-				}
-				else
-				{
-					putchar(((j / 10) % 10) + '0');
-					putchar(((j % 10) % 10) + '0');
-				}
+				putchar(((j / 10) % 10) + '0');
+				putchar((j % 10) + '0');
+
 				if ((i + j) < 197)
 				{
 					putchar(',');
