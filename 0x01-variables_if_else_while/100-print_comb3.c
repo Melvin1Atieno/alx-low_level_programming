b3000f01be6e8cd23b116db619c1f14954830b28
Melvin1Atieno <melvinatieno@gmail.com>
2022-10-18 10:57:42 -0700

    add combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			if (i != j)
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+
+				if ((i + j) < 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
