d4c6f4e66e5f3d9fec33027dbdeb493a4387e1fa
Melvin1Atieno <melvinatieno@gmail.com>
2023-01-10 12:04:01 -0800

    check for INT_MIN and INT_MAX

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,7 +7,27 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	if (n == INT_MIN)
+	{
+		_putchar('-');
+		_putchar('2');
+		n = INT_MAX;
+	}
+	else if (n == INT_MAX)
+	{
+		_putchar('2');
+		_putchar('1');
+		_putchar('4');
+		_putchar('7');
+		_putchar('4');
+		_putchar('8');
+		_putchar('3');
+		_putchar('6');
+		_putchar('4');
+		_putchar('7');
+		return;
+	}
+	else if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
