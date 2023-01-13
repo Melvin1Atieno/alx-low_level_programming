ebe53c0a32465b59ad3c7c2d54cf60fdb2aed095
Melvin1Atieno <melvinatieno@gmail.com>
2023-01-12 16:24:26 -0800

    own memset function. man memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: The memory
+ * @b: The constant
+ * @n: The number of bytes in s to fill
+ * @Return: Pointer to s
+ * Description: Fills the first n bytes of the memory area pointed by s with constant byte b
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int count;
+
+	count = 0;
+
+	while (count <= n)
+	{
+		*s = b;
+		s++;
+		count++;
+	}
+
+	s -= count;
+
+	return (s);
+}
