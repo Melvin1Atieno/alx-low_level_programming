4db6287b97569b8f6d994a533a01406bdf88f9c2
Melvin1Atieno <melvinatieno@gmail.com>
2023-01-12 16:31:30 -0800

    fix betty issues

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,8 +4,9 @@
  * @s: The memory
  * @b: The constant
  * @n: The number of bytes in s to fill
- * @Return: Pointer to s
- * Description: Fills the first n bytes of the memory area pointed by s with constant byte b
+ * Return: Pointer to s
+ * Description: Fills the first n bytes of the
+ * memory area pointed by s with constant byte b
  */
 char *_memset(char *s, char b, unsigned int n)
 {
