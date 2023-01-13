fc6ccaf29770163401ae93c04f798687d810d53a
Melvin1Atieno <melvinatieno@gmail.com>
2023-01-12 18:02:32 -0800

    find character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: The string
+ * @c: The character to find
+ * Return: Pointer to character occurence
+ * Description: Finds and returns the first occurence of
+ * a character in a string
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (0);
+}
